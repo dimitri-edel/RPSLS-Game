438bcc7e2e9665ea4cd8d62eb91b276c05d65856
dimitri-edel <dimitri.edel@gmx.net>
2025-06-21 13:17:31 +0200

    Add View class to gameV2.js and create game-v2.html

--- assets/jscript/gameV2.js
+++ assets/jscript/gameV2.js
@@ -11,8 +11,18 @@ const rules = [
     "Rock crushes scissors"
 ]
 
+// The following messages will be displayed at the top of the page. These are motivational phrases.
+const starting_message = "Welcome! May the force be with you!";
+const winning_message = "You are winning! Keep going!";
+const losing_message = "Do not give up! Luck never gives; it only lends.";
+const tie_message = "The game is tied. You can still win!";
+const win_message = "Congratulations! You did awsome! The force was on your side!";
+const loss_message = "Sorry! You have lost. Better luck next time!";
+const draw_message = "Not bad! Not bad, at all!";
+
+
 class Rock {
-    static image = "rock.webp";
+    static imageFileName = "rock.webp";
     static name = "rock";
     // Checks the outcome of playing it against Paper, Scissorrs, etc.
     // If Rock wins the return value is "win" else "loss" or "draw"
@@ -47,7 +57,7 @@ class Rock {
 }
 
 class Paper {
-    static image = "paper.webp";
+    static imageFileName = "paper.webp";
     static name = "paper";
     static checkOutcomeAgainst(oponentsPick) {
         let wins_against = [Rock, Spock];
@@ -80,7 +90,7 @@ class Paper {
 }
 
 class Scissors {
-    static image = "scissors.webp";
+    static imageFileName = "scissors.webp";
     static name = "scissors";
     static checkOutcomeAgainst(oponentsPick) {
         let wins_against = [Paper, Lizard];
@@ -112,7 +122,7 @@ class Scissors {
 }
 
 class Lizard {
-    static image = "lizard.webp";
+    static imageFileName = "lizard.webp";
     static name = "lizard";
     static checkOutcomeAgainst(oponentsPick) {
         let wins_against = [Spock, Paper];
@@ -144,7 +154,7 @@ class Lizard {
 }
 
 class Spock {
-    static image = "spock.webp";
+    static imageFileName = "spock.webp";
     static name = "spock";
     static checkOutcomeAgainst(oponentsPick) {
         let wins_against = [Scissors, Rock];
@@ -178,7 +188,7 @@ class Spock {
 // Player contains information on the player, such as the score and the current pick
 class Player {
     static score = 0;
-    static pick = none;
+    static pick = null;
 
     // Increases the current score by 1
     static increaseScore() {
@@ -186,7 +196,14 @@ class Player {
     }
 }
 
-class ComputerPlayer extends Player {
+class ComputerPlayer{
+    static score = 0;
+    static pick = null;
+
+    // Increases the current score by 1
+    static increaseScore() {
+        this.score++;
+    }
     // Computer picks a random move
     // Pick a ramdom move for the player. Used for the computer player.
     static pickRandom() {
@@ -256,11 +273,11 @@ class Game {
         result_message = this.userPlayer.currentPick.checkOutcomeMessageAgainst(this.computerPlayer.currentPick);
 
         // Asign score
-        if (result == win) {
+        if (result === "win") {
             this.userPlayer.increaseScore();
-        } else if (result == loss) {
+        } else if (result === "loss") {
             this.computerPlayer.increaseScore();
-        } else if (result == draw) {
+        } else if (result === "draw") {
             this.userPlayer.increaseScore();
             this.computerPlayer.increaseScore();
         }
@@ -295,6 +312,199 @@ class Game {
     }
 }
 
+/*
+    View contains all methods that display changes in the process of the game.
+    It also harbors the methods that will be called when the user clicks on an html element that has the onclick event listener.
+
+    PURPOSE:
+    Separate the game logic from the presentation logic.
+*/
+class View {    
+        // IDs of images whose setting need to be restored after every round
+    static restoreComputerImageId = null;
+    static restoreUserImageId = null;
+        // Copy of the HTML code of the settings-panel, so it can be reset later on
+    static initialSettingsHTML = "<label for='number-of-attempts-setting'>Number of possible attempts:</label><input id='number-of-attempts-setting' type='text'>";
+
+        // List of optionPickerIds
+    static optionPickerIds = ["user-rock-button", "user-paper-button",
+            "user-scissors-button", "user-lizard-button",
+            "user-spock-button", "computer-rock-button",
+            "computer-paper-button", "computer-scissors-button",
+            "computer-lizard-button", "computer-spock-button"
+        ];
+
+    
+    
+
+    // Applies changes to the html document to show the outcome of the current round
+    static displayOutComeResults(outcome, outcome_message, user, computer) {
+
+        let messageText = "";        
+        this.restoreComputerImageId = "";
+        this.restoreUserImageId = "";
+        switch (outcome) {
+            case "win":
+                messageText = "YOU WIN!";                
+                break;
+            case "loss":
+                messageText = "YOU LOSE!";                
+                break;
+            case "draw":
+                messageText = "DRAW!";
+                break;
+            default:
+                break;
+        }
+
+        document.getElementById("outcome-text").innerHTML = messageText;
+        document.getElementById("user-pick-image").src = "./assets/images/webp/"+game.userPlayer.currentPick.imageFileName;
+        document.getElementById("computer-pick-image").src = "./assets/images/webp/"+game.computerPlayer.currentPick.imageFileName;
+        document.getElementById("message-text").innerHTML = outcome_message;
+        document.getElementById("settings-panel").style = "display:none;";
+        document.getElementById("rounds-counter-panel").style = "display:block;";
+        document.getElementById("rounds-counter").innerHTML = game.currentRound;
+       
+        if(game.roundsLeft > 0) {
+            if(game.userPlayer.score > game.computerPlayer.score){
+                document.getElementById("message-display").innerHTML = winning_message;
+            }else if(game.userPlayer.score < game.computerPlayer.score){
+                document.getElementById("message-display").innerHTML = losing_message;
+            }else {
+                document.getElementById("message-display").innerHTML = tie_message;
+            }
+        }
+
+        if (game.roundsLeft <= 0) {
+            this.displayGameOver();
+        }
+    }
+
+    // Applies changes to the html document once the game is over
+    static displayGameOver() {
+        document.getElementById("outcome-text").innerHTML = "GAME OVER!";
+        let messageText = "";
+        if(game.computerPlayer.score > game.userPlayer.score){
+            messageText = "You have lost this game!";
+            document.getElementById("message-display").innerHTML = loss_message;
+        }else if(game.computerPlayer.score < game.userPlayer.score){
+            messageText = "You have won this game!";
+            document.getElementById("message-display").innerHTML = win_message;
+        }else {
+            messageText = "This game came up a draw!";
+            document.getElementById("message-display").innerHTML = draw_message;
+        }
+        document.getElementById("message-text").innerHTML = messageText;
+        this.clearOptionPickerEventListeners();
+    }
+
+    // Is triggered when the 'restart' icon is clicked
+    static startOverOnClick() {
+        game.startOver();
+        this.hookUpOptionPickerEventListeners();
+        document.getElementById("message-display").innerHTML = starting_message;
+        document.getElementById("outcome-text").innerHTML = "GO!";
+        document.getElementById("message-text").innerHTML = "Starting a new Game!";
+        document.getElementById("user-pick-image").src = "./assets/images/webp/question.webp";
+        document.getElementById("computer-pick-image").src = "./assets/images/webp/question.webp";
+        document.getElementById("settings-panel").style = "display:block;";
+        document.getElementById("rounds-counter-panel").style = "display:none;";
+    }
+
+    // Is triggered when the 'home' icon is clicked
+    static homeButtonOnClick() {
+        window.open("index.html", "_self");
+    }
+    // Removes onclick event listeners from the user option picker panel. Used when the game is over, to prevent the
+    // user from continueing the game. After that it becomes necessary to click the restart icon to start a new game.    
+    static clearOptionPickerEventListeners() {
+        const userOptionPickerIds = ["user-rock-button", "user-paper-button",
+            "user-scissors-button", "user-lizard-button",
+            "user-spock-button"
+        ];
+        document.getElementById(userOptionPickerIds[0]).removeEventListener("click", this.userRockOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[1]).removeEventListener("click", this.userPaperOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[2]).removeEventListener("click", this.userScissorsOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[3]).removeEventListener("click", this.userLizzardOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[4]).removeEventListener("click", this.userSpockOptionPickerOnClick);
+    }
+
+    // Sets event listeners on each all buttons that the user can click to play the game
+    static hookUpOptionPickerEventListeners() {
+        const userOptionPickerIds = ["user-rock-button", "user-paper-button",
+            "user-scissors-button", "user-lizard-button",
+            "user-spock-button"
+        ];
+        
+        document.getElementById(userOptionPickerIds[0]).addEventListener("click", this.userRockOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[1]).addEventListener("click", this.userPaperOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[2]).addEventListener("click", this.userScissorsOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[3]).addEventListener("click", this.userLizzardOptionPickerOnClick);
+
+        document.getElementById(userOptionPickerIds[4]).addEventListener("click", this.userSpockOptionPickerOnClick);
+    }
+
+    /* EventListners for the panel with buttons from which the user can pick his move */
+    static userRockOptionPickerOnClick() {
+        game.userMakesMove(Rock);
+    }
+
+    static userPaperOptionPickerOnClick() {
+        game.userMakesMove(Paper);
+    }
+
+    static userScissorsOptionPickerOnClick() {
+        game.userMakesMove(Scissors);
+    }
+
+    static userLizzardOptionPickerOnClick() {
+        game.userMakesMove(Lizard);
+    }
+
+    static userSpockOptionPickerOnClick() {
+        game.userMakesMove(Spock);
+    }
+
+    // Updates the score of each player and colors them accordingly
+    static updateScore(user, computer) {
+        let user_tag = document.getElementById("user-score");
+        let computer_tag = document.getElementById("computer-score");
+
+        // set colors to the numbers red for loss, green for win
+        if (user.score > computer.score) {
+            user_tag.style = "color: green;";
+            computer_tag.style = "color: red;";
+        } else if (user.score < computer.score) {
+            user_tag.style = "color: red;";
+            computer_tag.style = "color: green;";
+        } else {
+            user_tag.style = "color: green;";
+            computer_tag.style = "color: green;";
+        }
+        // update the score numbers
+        computer_tag.innerHTML = game.computerPlayer.score;
+        user_tag.innerHTML = game.userPlayer.score;
+    }
+
+    // Retrieves the number of rounds setting
+    static getNumberOfRoundsSetting() {
+        let val = document.getElementById("number-of-attempts-setting").value;
+        // If not a number set to default
+        if (isNaN(val)) {
+            val = 5;
+        }
+
+        return val;
+    }
+}
+
 function testAPick() {
     let pick = Rock;
     let oponentsPick = Scissors;
@@ -306,3 +516,12 @@ function testAPick() {
 
 testAPick();
 console.log("Test completed.");
+
+// Initialize global objects
+// The game object is initialized with 3 rounds as initial setting
+let game = Game;
+// The view object will be used to relay view events
+let view = View;
+view.hookUpOptionPickerEventListeners();
+// Set the initial message to the #message-display
+document.getElementById("message-display").innerHTML = starting_message;
